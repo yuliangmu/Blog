1b34d61abae780b493d4de0fdd9ec6d3a517507f
鱼梁木 <yuliangmu@hotmail.com>
2019-05-18 23:57:07 +0800

    添加新版 deepFreeze

--- utils/deepFreeze.js
+++ utils/deepFreeze.js
@@ -1,15 +1,23 @@
 /**
  * @desc: 深度冻结一个对象
- * @param {object} - 需要冻结的对象
+ * @param {obj} - 需要冻结的对象
  * @return: 传入的对象（深度冻结）
  */
 
-function deepFreeze(object) {
-  const propNames = Object.getOwnPropertyNames(object)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze#Freezing_arrays
+function deepFreeze(obj) {
+  const propNames = Object.getOwnPropertyNames(obj)
   for (let name of propNames) {
-    let value = object[name]
-    object[name] =
-      value && typeof value === 'object' ? deepFreeze(value) : value
+    let value = obj[name]
+    obj[name] = value && typeof value === 'object' ? deepFreeze(value) : value
   }
-  return Object.freeze(object)
+  return Object.freeze(obj)
 }
+
+// https://30secondsofcode.org/object#deepfreeze
+const deepFreeze2 = obj =>
+  Object.keys(obj).forEach(prop =>
+    !(obj[prop] instanceof Object) || Object.isFrozen(obj[prop])
+      ? null
+      : deepFreeze2(obj[prop])
+  ) || Object.freeze(obj)
